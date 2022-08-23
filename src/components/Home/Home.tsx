9f8bd9b49469f7a915ff0d39703914246329ab8f
Corey <hamhams86@gmail.com>
2022-08-23 12:12:35 -0700

    initial commit

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavBar } from '../Navbar'
+import { Helmet } from 'react-helmet'
+
+export const Home = () => {
+  return (
+    <>
+    <Helmet> 
+      <title>Carventory - Home</title>
+    </Helmet> 
+    <div>
+      Hello
+    </div>
+    </>
+  )
+}
---
+++ src/components/Navbar/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+
+export const NavBar = () => {
+  return (
+    <Navbar bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand href='#'>Carventory</Navbar.Brand> 
+        <Nav className='ms-auto'>
+          <Nav.Link href='/'>Home</Nav.Link>
+          <Nav.Link href='/profile'>Profile</Nav.Link>
+          <Nav.Link href='/cars'>Cars</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  )
+}
